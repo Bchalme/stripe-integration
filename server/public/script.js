4ba948cbe11d3a3c509549137bcdf7260ec011e5
Bchalme <brett@netos.one>
2023-10-18 13:58:11 -0400

    Finished on local host

--- server/public/script.js
+++ server/public/script.js
@@ -3,13 +3,23 @@ button.addEventListener("click", () => {
   fetch('/create-checkout-session', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      items: []
-
-    })
-
+      items: [
+        { id: 1, quantity: 3 },
+        { id: 2, quantity: 1 },
+      ],
+    }),  
   })
-  
+  .then(res => {
+    if (res.ok) return res.json()
+    return res.json().then(json => Promise.reject(json))
+  })
+  .then(({ url }) => {
+    window.location = url
+  })
+  .catch(e => {
+    console.error(e.error)
+  })
 })
